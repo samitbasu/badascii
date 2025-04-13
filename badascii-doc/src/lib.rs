4b5867ccc8e2d4ff2e468ee8807d809576e2879f
Samit Basu <basu.samit@gmail.com>
2025-04-13 12:40:51 -0700

    Sort out the proc macro.

--- badascii-doc/src/lib.rs
+++ badascii-doc/src/lib.rs
@@ -1,4 +1,4 @@
-use badascii::{Options, render::RenderJob, text_buffer::TextBuffer};
+use badascii::{render::RenderJob, text_buffer::TextBuffer};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{LitStr, parse_macro_input};
@@ -15,26 +15,10 @@ fn get_text_buffer(input: LitStr) -> TextBuffer {
 }
 
 #[proc_macro]
-pub fn badascii_smooth(input: TokenStream) -> TokenStream {
+pub fn badascii_formal(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
     let text_buffer = get_text_buffer(input);
-    let size = text_buffer.size();
-    let options = Options {
-        disable_multi_stroke: Some(true),
-        max_randomness_offset: Some(0.0),
-        roughness: Some(0.0),
-        ..Default::default()
-    };
-    let job = RenderJob {
-        width: (size.num_cols * 10) as f32,
-        height: (size.num_rows * 15) as f32,
-        num_cols: size.num_cols,
-        num_rows: size.num_rows,
-        text: text_buffer,
-        options,
-        x0: 0.0,
-        y0: 0.0,
-    };
+    let job = RenderJob::formal(text_buffer);
     let svg = badascii::svg::render(&job, "currentColor");
     let svg = format!("<p>{svg}</p>");
     quote!(#svg).into()
@@ -44,17 +28,7 @@ pub fn badascii_smooth(input: TokenStream) -> TokenStream {
 pub fn badascii(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
     let text_buffer = get_text_buffer(input);
-    let size = text_buffer.size();
-    let job = RenderJob {
-        width: (size.num_cols * 10) as f32,
-        height: (size.num_rows * 15) as f32,
-        num_cols: size.num_cols,
-        num_rows: size.num_rows,
-        text: text_buffer,
-        options: Options::default(),
-        x0: 0.0,
-        y0: 0.0,
-    };
+    let job = RenderJob::rough(text_buffer);
     let svg = badascii::svg::render(&job, "currentColor");
     let svg = format!("<p>{svg}</p>");
     quote!(#svg).into()
